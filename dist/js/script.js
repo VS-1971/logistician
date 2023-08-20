9097b4b68467035793d8748bbbf9c89970708215
serega <example@gmail.com>
2023-08-20 20:54:13 +0300

    second

---
+++ dist/js/script.js
@@ -0,0 +1,10 @@
+
+$('.carousel__inner').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow-left.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow-right.png"></button>'
+  });
